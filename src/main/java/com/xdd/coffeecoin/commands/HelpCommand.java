818fd00976e18f0f8b900c6cd52e782c5a04d861
woodcarver <woodcarvercello@gmail.com>
2018-08-29 21:39:04 +0800

    Update HelpCommand.java

--- src/main/java/com/xdd/coffeecoin/commands/HelpCommand.java
+++ src/main/java/com/xdd/coffeecoin/commands/HelpCommand.java
@@ -1,13 +1,16 @@
 package com.xdd.coffeecoin.commands;
 
+import java.util.List;
+
 /**
  * Created by qingying.xdd on 18/8/24.
  */
 public class HelpCommand {
-  public void execute(String[] args) {
-    System.out.println(String.format("Please input those command:" +
-            "help" +
-            "balance <address>" +
-            "log <address>"));
+  public Object execute(List<Object> args) {
+    return String.format("Please input those command: \n" +
+            "  - help \n" +
+            "  - wallet genkey \n" +
+            "  - balance <address> \n" +
+            "  - log <address>");
   }
 }
