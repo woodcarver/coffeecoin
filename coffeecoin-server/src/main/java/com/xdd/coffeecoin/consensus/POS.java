df36c0b5fd4d9e7cc222d9e74a7a1ec5810301df
woodcarver <woodcarvercello@gmail.com>
2018-09-07 17:58:33 +0800

    Update POS.java

--- coffeecoin-server/src/main/java/com/xdd/coffeecoin/consensus/POS.java
+++ coffeecoin-server/src/main/java/com/xdd/coffeecoin/consensus/POS.java
@@ -3,5 +3,20 @@ package com.xdd.coffeecoin.consensus;
 /**
  * Created by qingying.xdd on 18/8/20.
  */
-public class POS {
+public class POS implements Consensus {
+  private final static int ZERO_CNT_REQUIRE = 3;
+
+  public boolean isCorrectAnswer(byte[] hashResult) {
+    // counter 0 in hash
+    int cnt = 0;
+    for (int i = 0; i < hashResult.length; i++) {
+      if (hashResult[i] == 0) {
+        cnt++;
+      }
+    }
+    if (cnt > ZERO_CNT_REQUIRE) {
+      return true;
+    }
+    return false;
+  }
 }
