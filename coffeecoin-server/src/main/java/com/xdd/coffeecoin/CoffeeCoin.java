b73a2338332abf86f408b72c2149993fb344a931
woodcarver <woodcarvercello@gmail.com>
2018-09-10 21:26:35 +0800

    Update CoffeeCoin.java

--- coffeecoin-server/src/main/java/com/xdd/coffeecoin/CoffeeCoin.java
+++ coffeecoin-server/src/main/java/com/xdd/coffeecoin/CoffeeCoin.java
@@ -2,9 +2,10 @@ package com.xdd.coffeecoin;
 
 import com.xdd.coffeecoin.commands.Command;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.lang.reflect.InvocationTargetException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  * Created by qingying.xdd on 18/8/17.
@@ -14,18 +15,27 @@ public class CoffeeCoin {
 
   private static boolean isTerminated = false;
 
-  public static void main(String[] args) throws Exception {
-    System.out.println("Welcome to coffee coin world! All after all, " +
-            "enjoy your coffee first if you have one cup.^v^");
+  public static void main(String[] args) throws IOException {
+    int port = 7000;
+    // 在端口上创建一个服务器套接字
+    ServerSocket serverSocket = new ServerSocket(port);
+    // 监听来自客户端的连接, only receive one client
+    Socket socket = serverSocket.accept();
 
-    while(!isTerminated) {
-      System.out.print(TIP);
-      BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
-      String line = stdin.readLine();
-      String result = executeCmd(new Command(line));
+    DataInputStream dis =
+            new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+    DataOutputStream dos =
+            new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
 
-      System.out.println(result);
-    }
+    do {
+      String data = dis.readUTF();
+      System.out.println("服务器端收到数据为：" + data);
+      dos.writeUTF("reponse for :" + data);
+      dos.flush();
+    } while(dis.readByte() != 0);
+
+    socket.close();
+    serverSocket.close();
   }
 
   private static String executeCmd(Command clientCmd)
